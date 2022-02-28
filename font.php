a15f5905e371b5e61d8b814a16b5a40f1bf587a8
MiraclesI <100604492+MiraclesI@users.noreply.github.com>
2022-02-28 23:30:19 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+        <title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+        <style>
+            #box1{
+                width: 800px;
+                height: 350px;
+                background-color:lightblue
+            }
+            #box2{
+                width: 600px;
+                height: 150px;
+            }
+            body{
+                background-color: pink;
+            }
+        </style>
+    </head>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("FC Subject Rounded Bold [Non-commercial use].otf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("j.devilfont@Drift King Ex Demo.otf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">รวีโรจน์ มานะคิด เลขที่ 3 เลขประจำตัว 56978</h1>
+    <h1 id="font2">รวีโรจน์ มานะคิด เลขที่ 3 เลขประจำตัว 56978</h1>
+  </body>
+</html>
